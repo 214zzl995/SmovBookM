e800c78f51e08332087d1c6ce974c9fb0762f283
leri <dw99599@163.com>
2022-04-28 16:14:09 +0800

    添加视频播放器的lib包

---
+++ videoplayer/src/main/java/com/leri/videoplayer/PlayerSurface.kt
@@ -0,0 +1,24 @@
+package com.leri.videoplayer
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.exoplayer2.ui.StyledPlayerView
+
+@Composable
+fun PlayerSurface(
+    modifier: Modifier = Modifier,
+    onPlayerViewAvailable: (StyledPlayerView) -> Unit = {}
+) {
+    AndroidView(
+        factory = { context ->
+            StyledPlayerView(context).apply {
+                useController = false
+                onPlayerViewAvailable(this)
+            }
+        },
+        modifier = modifier
+    )
+}
+
+
---
+++ videoplayer/src/main/java/com/leri/videoplayer/VideoPlayerState.kt
@@ -0,0 +1,20 @@
+package com.leri.videoplayer
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class VideoPlayerState(
+    val isPlaying: Boolean = true,
+    val controlsVisible: Boolean = true,
+    val controlsEnabled: Boolean = true,
+    val gesturesEnabled: Boolean = true,
+    val fullScreen: Boolean = false,
+    val duration: Long = 1L,
+    val currentPosition: Long = 1L,
+    val secondaryProgress: Long = 1L,
+    val videoSize: Pair<Float, Float> = 1920f to 1080f,
+    val draggingProgress: DraggingProgress? = null,
+    val playbackState: PlaybackState = PlaybackState.IDLE,
+    val quickSeekAction: QuickSeekAction = QuickSeekAction.none()
+) : Parcelable
