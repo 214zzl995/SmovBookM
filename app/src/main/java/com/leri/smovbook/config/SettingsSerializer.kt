15bddfb6f3da4c506d6d32b2fb199c721da65a49
leri <214zzl995@gmail.com>
2022-08-10 21:02:14 +0800

    DataStore序列化测试

--- app/src/main/java/com/leri/smovbook/config/SettingsSerializer.kt
+++ app/src/main/java/com/leri/smovbook/config/SettingsSerializer.kt
@@ -1,10 +1,39 @@
 package com.leri.smovbook.config
 
+import android.content.Context
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.Serializer
+import androidx.datastore.dataStore
+import androidx.datastore.preferences.protobuf.InvalidProtocolBufferException
+import com.leri.smovbook.Settings
+import java.io.InputStream
+import java.io.OutputStream
+
 /**
- * @Description: XXX
+ * @Description: DataStore序列化测试
  * @author DingWei
  * @version 10/8/2022 下午7:13
-*/
+ */
+object SettingsSerializer : Serializer<Settings> {
+    override val defaultValue: Settings = Settings.getDefaultInstance()
 
+    override suspend fun readFrom(input: InputStream): Settings {
+        try {
+            return Settings.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
 
+    override suspend fun writeTo(
+        t: Settings,
+        output: OutputStream
+    ) = t.writeTo(output)
+}
+
+val Context.settingsDataStore: DataStore<Settings> by dataStore(
+    fileName = "settings.pb",
+    serializer = SettingsSerializer
+)
 
