ad1e8f8bd6fd6bf7261e2ac985a6eddbccbc3444
leri <214zzl995@gmail.com>
2022-08-20 17:27:14 +0800

    重构计划正在进行 请求拦截器 拦截url修改

--- app/src/main/java/com/leri/smovbook/models/network/SmovResponse.kt
+++ app/src/main/java/com/leri/smovbook/models/network/SmovResponse.kt
@@ -8,15 +8,15 @@ import com.leri.smovbook.models.entities.Smov
  * @Description: 主页数据返回
  * @author DingWei
  * @version 12/8/2022 上午11:49
- * @param page 页码
- * @param results 结果集
- * @param total_results 总结果数
+ * @param page_num 页码
+ * @param total 总结果数
  * @param total_pages 总页数
+ * @param list 结果集
  */
 @Immutable
 data class SmovResponse(
-    val page: Int,
-    val results: List<Smov>,
-    val total_results: Int,
-    val total_pages: Int
+    val page_num: Int,
+    val total: Int,
+    val total_pages: Int,
+    val list: List<Smov>
 ) : NetworkResponseModel
