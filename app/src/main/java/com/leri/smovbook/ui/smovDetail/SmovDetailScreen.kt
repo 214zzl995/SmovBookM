b75528e933a8997e8721fc3176f4114d8673f44a
leri <214zzl995@gmail.com>
2022-09-14 17:17:09 +0800

    JZplayer初始化

--- app/src/main/java/com/leri/smovbook/ui/smovDetail/SmovDetailScreen.kt
+++ app/src/main/java/com/leri/smovbook/ui/smovDetail/SmovDetailScreen.kt
@@ -1,11 +1,17 @@
 package com.leri.smovbook.ui.smovDetail
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import cn.jzvd.JZMediaSystem
+import cn.jzvd.JzvdStd
 import com.leri.smovbook.models.entities.Smov
 import com.leri.smovbook.ui.data.testDataSin
 
@@ -53,6 +59,19 @@ fun SmovDetailScreen(
                 .fillMaxSize(),
             contentAlignment = Alignment.TopStart
         ) {
+            //证明方案可行 需要模仿AgVideo 用jetpack compose 写一个播放器
+            //文档地址 http://jzvd.org/jzplayer/extends-usage.html
+            //项目地址 https://github.com/Jzvd/JZVideo/tree/develop/demo
+            AndroidView(factory = {
+                JzvdStd(it).apply {
+                    setUp(
+                        url,
+                        "饺子闭眼睛",
+                        JzvdStd.SCREEN_NORMAL
+                    )
+                }
+            })
+
 
         }
 
@@ -64,7 +83,13 @@ fun SmovDetailScreen(
 @Preview
 @Composable
 fun SmovDetailScreenPreview() {
-    SmovDetailScreen(smov = testDataSin, smovName = "SmovBook", onBack = { }, serverUrl = "", smovId = 10086)
+    SmovDetailScreen(
+        smov = testDataSin,
+        smovName = "SmovBook",
+        onBack = { },
+        serverUrl = "",
+        smovId = 10086
+    )
 }
 
 
